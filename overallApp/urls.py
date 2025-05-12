b077648d4aebb2c4941a4c446ac62c7a95b83f2c
vardhanguru <vardhanguru777@gmail.com>
2025-05-12 12:24:25 +0530

    updated urls.py updated views.py Updated base.html

--- overallApp/urls.py
+++ overallApp/urls.py
@@ -1,6 +1,11 @@
 
 from django.urls import path
 from overallApp.views import first_view, login_view
+
+
+
+
+
 urlpatterns = [
     path('first/', first_view, name='first'),
     path('login', login_view, name = 'login')
--- overallApp/views.py
+++ overallApp/views.py
@@ -2,6 +2,12 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 
+
+
+
+
+
+
 def first_view(request):
     data = {'name':'vardhan',
             'fruits':['apple','grapes'],
