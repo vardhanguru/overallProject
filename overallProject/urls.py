33c1d78adc5e7b54b503b9d6cd9d6c26b8b6f352
vardhanguru <68608658+vardhanguru@users.noreply.github.com>
2025-05-12 12:43:28 +0530

    Update urls.py

--- overallProject/urls.py
+++ overallProject/urls.py
@@ -21,5 +21,7 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('overallapp/', include('overallApp.urls')),
-    path('secondapp/', include('secondApp.urls'))
+    path('secondapp/', include('secondApp.urls')),
+    path('thirdapp/', include('thidrApp.urls')),
+    
 ]
